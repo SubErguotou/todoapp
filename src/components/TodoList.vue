c6f526763aab29961d143547410b1daa3eac454a
SubErguotou <fks12301818@163.com>
2021-08-30 16:29:12 +0800

    模块化完成

---
+++ src/components/TodoList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="todo-list">
+      <TodoListitemVue />
+  </div>
+</template>
+
+<script>
+import TodoListitemVue from './TodoListitem.vue';
+
+export default {
+    name: "TodoList",
+    components: {
+        TodoListitemVue
+    }
+};
+</script>
+
+<style lang="scss" scope>
+.todo-list {
+  display: grid;
+  row-gap: 14px;
+}
+</style>
